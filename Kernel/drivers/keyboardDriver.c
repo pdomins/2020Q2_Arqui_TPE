5ddb658b535d16785ecff8e70498ac3412ae3fdf
Paula Domingues <pdomingues@itba.edu.ar>
2020-10-25 19:42:54 -0300

    keyboard Driver works

--- Kernel/drivers/keyboardDriver.c
+++ Kernel/drivers/keyboardDriver.c
@@ -3,13 +3,32 @@
 
 #include <videoDriver.h>
 
-#define KEYS 59 // o 60?
-#define MIN_SCAN 0 MAX_SCAN 59 
+#define KEYS 59 // o 60? continuara... TAL VEZ? :D
+#define MIN_SCAN 0
+#define MAX_SCAN 59 
+
+#define ESC 0x01
+#define BACKSPACE 0x0E
+#define TAB 0x0F
+#define ENTER 0x1C
+#define L_CTRL 0x1D
+#define L_SHIFT 0x2A
+#define R_SHIFT 0x36
+#define L_ALT 0x38
+#define CAPS_LOCK 0x3A
+#define RELEASED_KEY 0x80
+#define MAX_PRESSED_KEY 0X7F
+
+#define SPECIAL_KEY (ESC||BACKSPACE||TAB||ENTER||L_CTRL||L_SHIFT||R_SHIFT||L_ALT||CAPS_LOCK)
+
+#define IS_ALPHA(c) c >= 'a' && c <= 'z'  
+
+
 char buffer[5];
 int prev = 0;
 int curr = 0; 
 static char pressCodes[KEYS][2] = {{0, 0}, {0, 0}, {'1', '!'}, {'2', '@'},
- {'3', '#'}, {'4', '$'}, {'5', '%'}, {'6', '^'}, {'7', '&'}, 
+{'3', '#'}, {'4', '$'}, {'5', '%'}, {'6', '^'}, {'7', '&'}, 
 {'8', '*'}, {'9', '('}, {'0', ')'}, {'-', '_'}, {'=', '+'},
 {'\b','\b'}, {'\t','\t'}, {'q', 'Q'}, {'w', 'W'},{'e', 'E'},
 {'r', 'R'}, {'t', 'T'}, {'y', 'Y'}, {'u', 'U'}, {'i', 'I'}, 
@@ -24,15 +43,37 @@ static char pressCodes[KEYS][2] = {{0, 0}, {0, 0}, {'1', '!'}, {'2', '@'},
 static int shiftPressed = 0;
 static int blockMayus = 0;
 
+int isSpecialKey(int scanCode){
+    return scanCode == ESC || scanCode == BACKSPACE || scanCode == ENTER || scanCode == L_CTRL ||
+            scanCode == L_SHIFT || scanCode == R_SHIFT || scanCode == L_ALT || scanCode == CAPS_LOCK;
+}
+
+
 void keyboard_management(){
     int scan_code = read_keyboard();
     prev = curr;
-    if(scan_code == 0x2A || scan_code == 0x36) {
-        shiftPressed = 1;
-    } else if(scan_code == (0x2A + 0x80) || scan_code == (0x36 + 0x80)) {
-        shiftPressed = 0;
-    } else if(scan_code <= 0x7F) {
-        buffer[curr++] = pressCodes[scan_code][shiftPressed];
+    switch(scan_code) {
+        case L_SHIFT:
+        case R_SHIFT:
+            shiftPressed = 1; 
+            break;
+        case L_SHIFT + RELEASED_KEY:
+        case R_SHIFT + RELEASED_KEY:
+            shiftPressed = 0; 
+            break;
+        case CAPS_LOCK:
+            blockMayus = blockMayus? 0:1;
+            break;
+        case L_CTRL:   
+        case L_ALT:
+            break;
+    }
+    if(scan_code <= MAX_PRESSED_KEY && !isSpecialKey(scan_code)) {
+        int secondChar = shiftPressed;
+        if(IS_ALPHA(pressCodes[scan_code][0])){
+            secondChar = blockMayus ? 1 - shiftPressed: shiftPressed;
+        }
+        buffer[curr++] = pressCodes[scan_code][secondChar];
         curr = curr % 5;
         printLatest();
     }
